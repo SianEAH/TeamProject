b625933a21e6561bd96dbc4fda9345c6d361e726
SianEAH <149701097+SianEAH@users.noreply.github.com>
2026-02-07 15:41:18 +0000

    Extra Folder Added for Organisation

---
+++ EscapeRoomApp/src/escaperoomapp/Puzzles/Rooms/CustomerServicePuzzle.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package escaperoomapp.Puzzles.Rooms;
+
+/**
+ *
+ * @author siane
+ */
+public class CustomerServicePuzzle {
+    //data members
+    
+    //constructor/s
+    
+    //setters: one for each input
+    
+    //methods
+    
+    //getters: one for each output
+    
+    //my other methods
+}
