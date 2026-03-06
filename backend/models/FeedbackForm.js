ac4d7fd9490b67329c4ddb964843d633b84bd50d
SianEAH <149701097+SianEAH@users.noreply.github.com>
2026-03-06 09:25:34 +0000

    Various little changes

--- backend/models/FeedbackForm.js
+++ backend/models/FeedbackForm.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const FeedbackSchema = new mongoose.Schema({
-    time: { type: String, required: true },
+    time: { type: String, required: true }, //maybe type time?
     score: { type: Number, required: true },
     name: { type: String, required: true },
     passOrFail: { type: String, required: true }
--- js/certificate.js
+++ js/certificate.js
@@ -1,3 +1,5 @@
+import QRCode from "qrcode";
+
 function showCertificate(certData) {
   document.getElementById("employeeName").textContent = certData.employeeName; /*returns the text of this element*/
   document.getElementById("moduleName").textContent = certData.moduleName;
@@ -10,16 +12,40 @@ function showCertificate(certData) {
 }
 
 /*when we click close, the cert will close*/
-document.getElementById("closeCertificateBTN")
-  .addEventListener("click", () => {
-    document.getElementById("certificate").classList.add("hidden");
+document.addEventListener("DOMContentLoaded", () => { //wait until the DOM has loaded
+
+  document
+    .getElementById("closeCertificateBTN")
+    .addEventListener("click", () => {
+      document.getElementById("certificate").classList.add("hidden");
   });
 
-/*Generating the QR code: NEEDS COMPLETING*/
-function generateQRCode(certId) {
+  document
+    .getElementById("downloadCertificateBTN")
+    .addEventListener("click", downloadCertificate);
+
+});
+
+//https://www.npmjs.com/package/qrcode
+function generateQRCode(certificateID) {
   const qrContainer = document.getElementById("qrCode");
   qrContainer.innerHTML = ""; /*make sure it's empty*/
 
+  const canvas = document.createElement("canvas"); //create a canvas for the QR Code
+
+  QRCode.toCanvas(canvas, certificateID, { width: 120 }, function (error) {
+    if (error) console.error(error); //error message
+  });
+
+  qrContainer.appendChild(canvas); //append the QR code to the canvas QR container
   
-  
+}
+
+//Download button
+document
+  .getElementById("downloadCertificateBTN")
+  .addEventListener("click", downloadCertificate);
+
+function downloadCertificate() {
+  window.print(); //https://www.w3schools.com/jsref/met_win_print.asp
 }
