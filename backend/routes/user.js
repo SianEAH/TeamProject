1ba2a94de5da10a444f910048e0e72fe93078b52
SianEAH <149701097+SianEAH@users.noreply.github.com>
2026-04-01 21:32:21 +0100

    Feature added to Employee dashboard to say status
    Will say whether module is incomplete, in progress or complete. Changes made to employeedashboard, user.js, dashboard.css, dashboard.js, employee.js.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -27,4 +27,82 @@ router.get('/certificate/:moduleName', async (req, res) => {
     }
 });
 
+//update module status route dynamically
+//Sian
+router.get('/', async (req, res) => {
+    try {
+        const referenceID = req.query.referenceID;
+
+        const user = await Employee.findOne({ referenceID });
+
+        //error handling
+        if (!user) return res.status(404).json({ msg: "User not found" });
+
+        let modules = user.completedModules; //let the modules = to the user's completed modules
+
+        //issue discovered by created account's module data type
+        //has to be an array
+        if(!Array.isArray(modules)) {
+            modules = modules ? [modules] : []; //check if it's an object or if it's empty
+        }
+
+        //the default modules in the Array
+        const defaultModules = ["Customer Service", "Fire Safety", "First Aid"];
+
+        defaultModules.forEach(name => {
+            if(!modules.find(m => m.moduleName === name)) {
+                modules.push({ moduleName: name, status: "incomplete"}); //push the status for the module names, to overwrite what's already been created
+            }
+        });
+
+        res.json({ completed: modules }); //sending my data back
+
+    } catch (err) { //error handling
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
+router.post('/start', async (req, res) => {
+    try {
+        const { moduleName, referenceID } = req.body;
+
+        const user = await Employee.findOne({ referenceID });
+
+        //error handling
+        if (!user) return res.status(404).json({ msg: "User not found" });
+
+        let modules = []; //modules starts as an empty Array
+
+        //Checking what type it is in order to send the data (Array/Object)
+        if(Array.isArray(user.completedModules)) {
+            modules = user.completedModules;
+        } else if(user.completedModules) {
+            modules = [user.completedModules];
+        }
+
+        //Does the module already exist
+        const existing = modules.find(m => m.moduleName === moduleName);
+
+        if (existing) {
+            existing.status = "in-progress"; //if it does, push this status
+        } else {
+            modules.push({ moduleName, status: "in-progress"}); //if it doesn't, push the module & the status
+        }
+
+        user.completedModules = modules;
+        user.markModified('completedModules'); //letting the database know there is a change
+                                               //Mongoose: markModified ensures changes to nested objects/arrays are saved
+                                               //https://mongoosejs.com/docs/5.x/docs/api/document.html#document_Document-markModified
+
+        await user.save();
+
+        res.json({ message: "Updated"});
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
 module.exports = router;
